a6f942a8685cc8db8b1cf907aaef855166331892
nikolai-vasilyev <vasiljev.nickolo@yandex.ru>
2024-10-03 20:51:18 +0300

    HW_02_correction

--- img/02/src/locals.tf
+++ img/02/src/locals.tf
@@ -1,4 +1,9 @@
 locals {
-  vm_db_name = var.vm_db_name
-  vm_web_name = var.vm_web_name
+  name1 = "netology"
+  name2 = "develop"
+  name3 = "platform"
+  name4 = "db"
+  name5 = "web"
+  vm_db_name = "${local.name1} - ${local.name2} - ${local.name3} - ${local.name4}"
+  vm_web_name = "${local.name1} - ${local.name2} - ${local.name3} - ${local.name5}"
 }
