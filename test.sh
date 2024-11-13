fb86446ffca234713e0c6abb34f9547ab405736b
foxbeerxxx <tatarincev0810@gmail.com>
2024-11-14 00:12:57 +0300

    1 to dev

--- test.sh
+++ test.sh
@@ -2,3 +2,9 @@ ind -name "*.xlsx" | while read f; do
   fn="$(basename "$f")"; 
   cp "${f}" "/home/vskibenko/tmp/${fn,,}";
 done
+cp "A.txt" "dst/a.txt";
+cp "b.txt" "dst/b.txt";
+cp "c D.txt" "dst/c d.txt"
+
+*********************
+find -name «*.xlsx» | tr [:upper:] [:lower:] | cp /home/vskibenko/tmp/.
