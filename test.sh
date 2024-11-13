a5c7436390586e11c3bbb7f5346e26bb1f15dd3a
foxbeerxxx <tatarincev0810@gmail.com>
2024-11-14 00:19:15 +0300

    1 dev

--- test.sh
+++ test.sh
@@ -8,3 +8,5 @@ cp "c D.txt" "dst/c d.txt"
 
 *********************
 find -name «*.xlsx» | tr [:upper:] [:lower:] | cp /home/vskibenko/tmp/.
+#cp --parent /var/www/html/upload/x7v/b7f/b7f8abcf54d20d7a482e56405b3b51ec.jpg  /home/user/newsite/data/news/oldimg/
+cp /var/www/html/upload/x7v/b7f/b7f8abcf54d20d7a482e56405b3b51ec.jpg  /home/user/newsite/data/news/oldimg/x7v/b7f/b7f8abcf54d20d7a482e56405b3b51ec.jpg
