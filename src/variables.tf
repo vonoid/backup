d4a85d94d7982184672cb14fb45a4cb69d60bcbb
tud777777 <tud777777@yandex.ru>
2024-11-13 17:15:17 +0300

    123

---
+++ src/variables.tf
@@ -0,0 +1,40 @@
+###cloud vars
+
+
+variable "cloud_id" {
+  type        = string
+  default = "b1gsvfdtubarjbp5hrcg"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default = "b1gbq0odhebh8pnqgnm9"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC+GmAvbNPxQaNyfLqOrUq+Nq/7DYqqy3ByaSFCb2q6W user@WIN-ECB0J3KQ1RA"
+  description = "ssh-keygen -t ed25519"
+}
